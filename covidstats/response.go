c5fff8b936cb2f624451473e5f373adfa3bd01d6
imran.beg <imran.beg@telekom-digital.com>
2021-11-09 12:23:09 +0530

    pushed

--- covidstats/response.go
+++ covidstats/response.go
@@ -20,13 +20,8 @@ type StateData struct {
 	Active          string `json:active`
 	Confirmed       string `json:confirmed`
 	Deaths          string `json:deaths`
-	DeltaConfirmed  string `json:deltaconfirmed`
-	DeltaDeaths     string `json:deltadeaths`
-	DeltaRecovered  string `json:deltarecovered`
 	LastUpdatedTime string `json:lastupdatedtime`
 	Recovered       string `json:recovered`
 	MigratedOther   string `json:migratedother`
 	State           string `json:state`
-	StateCode       string `json:statecode`
-	StateNotes      string `json:statenotes`
 }
